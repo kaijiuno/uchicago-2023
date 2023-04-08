ec3778aa8962ebba3205b1326456ea274a95d7da
kaijiuno <64614098+kaijiuno@users.noreply.github.com>
2023-04-07 20:56:27 -0400

    updated final

--- case3/allocate.py
+++ case3/allocate.py
@@ -15,3 +15,17 @@ def allocate_portfolio(asset_prices):
     n_assets = len(asset_prices)
     weights = np.repeat(1 / n_assets, n_assets)
     return weights
+
+def grading(testing): #testing is a pandas dataframe with price data, index and column names don't matter
+    weights = np.full(shape=(len(testing.index),10), fill_value=0.0)
+    for i in range(0,len(testing)):
+        unnormed = np.array(allocate_portfolio(list(testing.iloc[i,:])))
+        positive = np.absolute(unnormed)
+        normed = positive/np.sum(positive)
+        weights[i]=list(normed)
+    capital = [1]
+    for i in range(len(testing) - 1):
+        shares = capital[-1] * np.array(weights[i]) / np.array(testing.iloc[i,:])
+        capital.append(float(np.matmul(np.reshape(shares, (1,10)),np.array(testing.iloc[i+1,:]))))
+    returns = (np.array(capital[1:]) - np.array(capital[:-1]))/np.array(capital[:-1])
+    return np.mean(returns)/ np.std(returns) * (252 ** 0.5), capital, weights
